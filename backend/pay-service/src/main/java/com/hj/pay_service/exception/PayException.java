97323f205014b72cf36f7633840a6b050e09cc72
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-21 17:41:47 +0900

    ㅏ

--- backend/pay-service/src/main/java/com/hj/pay_service/exception/PayException.java
+++ backend/pay-service/src/main/java/com/hj/pay_service/exception/PayException.java
@@ -1,9 +1,9 @@
 package com.hj.pay_service.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class PayException extends RuntimeException {
 
--- backend/pay-service/src/test/java/com/hj/pay_service/PayServiceApplicationTests.java
+++ backend/pay-service/src/test/java/com/hj/pay_service/PayServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.hj.pay_service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.hj.pay_service.service.PayService;
 
 @SpringBootTest
 class PayServiceApplicationTests {
 
+	@MockitoBean
+    private DataSource dataSource; 
+	
+	@Autowired
+    private PayService payService;
+
 	@Test
 	void contextLoads() {
+		 assertNotNull(payService);
 	}
 
 }
