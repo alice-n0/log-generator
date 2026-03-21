a53b21dd86d5dfacbe0d89168465180dbd1b7e9c
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-21 18:11:29 +0900

    f

--- backend/order-service/src/test/java/com/hj/order_service/OrderServiceApplicationTests.java
+++ backend/order-service/src/test/java/com/hj/order_service/OrderServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.hj.order_service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.hj.order_service.service.OrderService;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
+	@MockitoBean
+	private WebClient webClient;
+
+	@Autowired
+	private OrderService orderService;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(orderService);
 	}
 
 }
