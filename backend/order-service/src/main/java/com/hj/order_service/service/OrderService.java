35b1fc8d659ed8563d17eb75fb71c142410ba662
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-21 16:45:42 +0900

    ㅇ

--- backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
+++ backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.hj.order_service.service;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,7 +11,7 @@ import java.time.Duration;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    @Value("${PAYMENT_SERVICE_URL}")
+    @Value("${PAY_SERVICE_URL}")
     private String payServiceUrl;
 
     private final WebClient webClient;
@@ -20,15 +19,15 @@ public class OrderService {
     public String createOrder(String userId) {
 
         String result = webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                    .path(payServiceUrl)
-                    .queryParam("userId", userId)
-                    .build())
-            .retrieve()
-            .bodyToMono(String.class)
-            .timeout(Duration.ofSeconds(2)) // ⏱ timeout
-            .onErrorMap(e -> new RuntimeException("Payment API failed", e))
-            .block();
+                .uri(uriBuilder -> uriBuilder
+                        .path(payServiceUrl)
+                        .queryParam("userId", userId)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(2)) // ⏱ timeout
+                .onErrorMap(e -> new RuntimeException("Payment API failed", e))
+                .block();
 
         return "Order created for user " + userId + ", pay status: " + result;
     }
