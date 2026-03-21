676524047679812121d38f9729e1dd5d53db0139
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-21 16:43:24 +0900

    ?

--- backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
+++ backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
@@ -1,10 +1,13 @@
 package com.hj.order_service.service;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
