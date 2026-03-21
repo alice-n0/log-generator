f71dfdf04cf3f3b4f9a1681a169c6ceb4fffcb6f
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-21 16:34:21 +0900

    ㅇ

--- backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
+++ backend/order-service/src/main/java/com/hj/order_service/service/OrderService.java
@@ -2,7 +2,7 @@ package com.hj.order_service.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +12,21 @@ public class OrderService {
     @Value("${PAYMENT_SERVICE_URL}")
     private String payServiceUrl;
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     public String createOrder(String userId) {
-        // pay service 호출
-        String result = restTemplate.getForObject(payServiceUrl + "?userId=" + userId, String.class);
+
+        String result = webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                    .path(payServiceUrl)
+                    .queryParam("userId", userId)
+                    .build())
+            .retrieve()
+            .bodyToMono(String.class)
+            .timeout(Duration.ofSeconds(2)) // ⏱ timeout
+            .onErrorMap(e -> new RuntimeException("Payment API failed", e))
+            .block();
+
         return "Order created for user " + userId + ", pay status: " + result;
     }
 }
