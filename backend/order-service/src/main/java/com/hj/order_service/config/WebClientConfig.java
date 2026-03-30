33dad50477a132c1a616a87619640c876034529a
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-30 16:32:35 +0900

    WebClient.Builder로 변경

--- backend/order-service/src/main/java/com/hj/order_service/config/WebClientConfig.java
+++ backend/order-service/src/main/java/com/hj/order_service/config/WebClientConfig.java
@@ -1,18 +1,17 @@
 package com.hj.order_service.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@EnableConfigurationProperties(PayServiceProperties.class)
 public class WebClientConfig {
+
     @Bean
-    public WebClient payWebClient(PayServiceProperties prop) {
-        return WebClient.builder()
+    public WebClient payWebClient(WebClient.Builder builder,
+                                 PayServiceProperties prop) {
+        return builder
                 .baseUrl(prop.getBaseUrl())
                 .build();
     }
-
 }
