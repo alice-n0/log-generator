632d902a7728c5aa894bfb6b8373a4e68d89c42c
alice-n0 <hyeonjin5012@pukyong.ac.kr>
2026-03-30 16:42:24 +0900

    d

--- backend/order-service/src/main/java/com/hj/order_service/config/PayServiceProperties.java
+++ backend/order-service/src/main/java/com/hj/order_service/config/PayServiceProperties.java
@@ -1,10 +1,12 @@
 package com.hj.order_service.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "pay.service")
 public class PayServiceProperties {
     private String baseUrl;
